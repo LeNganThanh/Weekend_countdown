10f8156eac326b3808e0b0d5837e1e8282dd5c9e
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-08-08 13:44:21 +0200

    first weekend countdown commit

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+const body = document.querySelector("body");
+body.style.fontSize = "2rem";
+body.style.marginLeft = "5rem";
+const form = document.querySelector("form");
+
+const title = document.getElementById("para");
+
+const useName = document.getElementById("useName");
+useName.style.width = "25rem";
+useName.style.height = "3rem";
+useName.style.fontSize = "1.5rem";
+
+const today = new Date();
+const dayArr = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const date = dayArr[today.getDay()];
+const goBtn = document.getElementById("submitBtn");
+goBtn.style.width = "5rem";
+goBtn.style.height = "3rem";
+goBtn.style.fontSize = "1.3rem";
+goBtn.style.borderRadius = "5px";
+goBtn.style.marginLeft = "20px";
+
+goBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  title.textContent = `Hello ${useName.value}. Today is ${date}. Only ${
+    5 - today.getDay()
+  } days left until weekend!`;
+});
